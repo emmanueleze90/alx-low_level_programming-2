a2e9f00a5a06cd79678e7009077c3426ef0a5e53
Sarycode <abdulrahmanshopade@gmail.com>
2022-10-13 15:38:07 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/*
+ * main(void)-serves as the entry point of every code
+ * return:by default returns a zero value
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
