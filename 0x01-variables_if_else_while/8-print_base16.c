3875d96b5ebb32059c8d2c07c6eefe5802c5ee28
Sarycode <abdulrahmanshopade@gmail.com>
2022-10-15 15:35:57 -0700

    print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char hexvalue[] = "0123456789abcdef";
+
+	for (i = 0; i <= 16; i++)
+		putchar(hexvalue[i]);
+	putchar('\n');
+	return (0);
+}
