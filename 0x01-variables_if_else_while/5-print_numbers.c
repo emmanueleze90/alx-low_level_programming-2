be8da74bbab2ded9f269a434907b013c56b46120
Sarycode <abdulrahmanshopade@gmail.com>
2022-10-14 18:42:30 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int number;
+
+	number = 0;
+	while (number < 10)
+	{
+		printf("%d\n", number);
+		number++;
+	}
+	return (0);
+}
